6a766bf03b3a17eb643b2e8f626780a0e509196f
Joseph Huntley <80273102+JosephHuntley@users.noreply.github.com>
2023-01-05 16:43:22 -0500

    Delete src directory

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import Navbar from './Navbar';
-import Homepage from './Homepage';
-import News from './News';
-import Exchanges from './Exchanges';
-import CryptoDetails from './CryptoDetails';
-import Cryptocurrencies from './Cryptocurrencies';
-
-export { Navbar, Homepage, News, Exchanges, CryptoDetails, Cryptocurrencies };
